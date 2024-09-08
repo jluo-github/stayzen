78d978625d0708446f805cc2ae1925d6e4ac86e6
jluo <jluo8@wgu.edu>
2024-09-08 09:23:02 -0700

    route

--- app/api/supabase/route.ts
+++ app/api/supabase/route.ts
@@ -14,21 +14,21 @@ export async function GET(req: NextRequest, res: NextResponse) {
   }
 }
 
-
 export async function POST(req: NextRequest, res: NextResponse) {
+  const name = `CuteCat-${Math.ceil(Math.random() * 999)}`;
   try {
     const testProfile = await db.testProfile.update({
-      where:{
-        id: "4d9cb566-f845-4b9b-a131-7d22fb766220"
+      where: {
+        id: "4d9cb566-f845-4b9b-a131-7d22fb766220",
       },
       data: {
-        name: "Cute Cat",
+        name,
       },
     });
 
     return NextResponse.json({ message: "Supabase updated successfully", testProfile });
   } catch (error) {
     console.error("Error creating Supabase record:", error);
-    NextResponse.error();
+    return NextResponse.json({ error: "Error updating Supabase record" }, { status: 500 });
   }
 }
